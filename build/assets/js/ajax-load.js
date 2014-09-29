821b5dcd52f0c0e612d1c1bc5340cbe6ce71b356
秋知 <benfchen0418@gmail.com>
2014-09-29 14:08:11 +0800

    demo page done

--- build/assets/js/ajax-load.js
+++ build/assets/js/ajax-load.js
@@ -1,27 +1,9 @@
-KISSY.use('node,tabs,io', function(S, Node, Tabs, Io){
+KISSY.use('node,tabs,io,util', function(S, Node, Tabs, Io,Util){
 	var $ = Node.all;
-
-	function initDisqusThread(){
-		if((window.location.hostname.indexOf("kissyui.com")!=-1 )&& window.localStorage.getItem("kissy-commment")!="0"){
-		    /* * * CONFIGURATION VARIABLES: EDIT BEFORE PASTING INTO YOUR WEBPAGE * * */
-		    var disqus_shortname = 'kissy-docs'; // required: replace example with your forum shortname
-
-		    // The following are highly recommended additional parameters. Remove the slashes in front to use.
-		     //var disqus_identifier = '/anim';
-		     //var disqus_url = window.location;
-
-		    /* * * DON'T EDIT BELOW THIS LINE * * */
-		    (function() {
-		        var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
-		        dsq.src = 'http://' + disqus_shortname + '.disqus.com/embed.js';
-		        (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
-		    })();
-		    }
-	}
 
 	function editDemoOnlineInit(){
 		$('.editor').each(function(item$){
-			var demoHtml = S.unEscapeHTML(item$.html());
+			var demoHtml = Util.unEscapeHTML(item$.html());
 			var iframeWindow = item$.parent('.ks-tabs-body').all('.output')[0].contentWindow;
 			iframeWindow.document.body.innerHTML = '';
 			iframeWindow.document.write(demoHtml);
@@ -38,7 +20,7 @@ KISSY.use('node,tabs,io', function(S, Node, Tabs, Io){
 		    	srcNode : item$,
 		    	listeners:{
 		            afterSelectedTabChange:function(e){
-		            	if(e.newVal.get('content') === 'Output'){ //如果是切换到Output
+		            	if(e.newVal.$el.html() === 'Output'){ //如果是切换到Output
 		            		var iframeWindow = item$.all('.ks-tabs-body .output')[0].contentWindow;
 		            		iframeWindow.document.body.innerHTML = '';
 		    				iframeWindow.document.write(iframeWindow.editor.getValue());
@@ -65,72 +47,29 @@ KISSY.use('node,tabs,io', function(S, Node, Tabs, Io){
 	//demo页面初次加载时先初始化
 	window.location.href.indexOf('/5.0/demos') > -1 && editDemoOnlineInit();
 
-	//初始化评论功能
-	initDisqusThread();
-
 	// if(location.href.indexOf('5.0/guides') > -1){
-		var sidebar$ = $('#sidebar'),
-			sidebarLeft = sidebar$.offset().left,
-			sidebarTop = sidebar$.offset().top,
-			sidebarWidth = sidebar$.innerWidth();
-		window.onscroll = function(){
-			var scrollTop = document.body.scrollTop;
-			if(scrollTop > sidebarTop){
-				sidebar$.css({
-					position : 'fixed',
-					left : sidebarLeft,
-					top : 0,
-					width : sidebarWidth,
-					marginTop : '0px'
-				});
-			}else{
-				sidebar$.css({
-					position : '',
-					left : '0px',
-					marginTop : '100px'
-				});
-			}
-		}
-	// }
-
-	/* 这段本来是做无刷新页面加载的，但是边栏增加了标题导航后几乎整个页面都要刷新了，所以暂时去掉这功能*/
-	// if(window.history.pushState && window.sessionStorage && window.history.replaceState){
-	// 	function updateMainCotent(href){
-	// 		Io.get(href,{
-	//             r : Math.random()
-	//         },function(res){
-	//             var reg = /(<div.+id="main-content">[\s\S]+)<div.+id="sidebar"/;
-	//             var newMainContentHtml = res.match(reg)[1],
-	//                 newMainContentDom$ = $(newMainContentHtml);
-
-	//             $('#main-content').replaceWith(newMainContentDom$);
-
-	//             //更新sidebar的api链接
-	//             var newApiSrc = $('#apilink').attr('href');
-	//             $('#sidebar .link-apidocs').attr('href',newApiSrc);
-
-	//      		if(href.indexOf('/demos') > -1){  //如果跳到的是demo页面
-	//      			editDemoOnlineInit();
-	//      		}
-	//      		initDisqusThread();
-	//         });
-	// 	}
-
-	//     $('#sidebar .ajax-load a').on('click', function(ev){
-	//         ev.halt();
-	//         var target$ = $(ev.currentTarget),
-	//             href = target$.attr('href').replace(/\\/g,'/');
-	//         updateMainCotent(href);
-	//         window.history.pushState({ url : href },null,href);
-	//     });
-
-	//     window.onpopstate = function(event){  //浏览器后退
-	//     	if(event.state){
-	//     		updateMainCotent(event.state.url);
-	//     	}
-	//     }
-	    
-	//     history.replaceState({ url : location.href}, null, location.href);
+		// var sidebar$ = $('#sidebar'),
+		// 	sidebarLeft = sidebar$.offset().left,
+		// 	sidebarTop = sidebar$.offset().top,
+		// 	sidebarWidth = sidebar$.innerWidth();
+		// window.onscroll = function(){
+		// 	var scrollTop = document.body.scrollTop;
+		// 	if(scrollTop > sidebarTop){
+		// 		sidebar$.css({
+		// 			position : 'fixed',
+		// 			left : sidebarLeft,
+		// 			top : 0,
+		// 			width : sidebarWidth,
+		// 			marginTop : '0px'
+		// 		});
+		// 	}else{
+		// 		sidebar$.css({
+		// 			position : '',
+		// 			left : '0px',
+		// 			marginTop : '100px'
+		// 		});
+		// 	}
+		// }
 	// }
 });
 
